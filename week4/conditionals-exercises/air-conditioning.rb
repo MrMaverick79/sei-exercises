a499fdb2b4481e3366ee8c63f7eb494782910c33
Brendan Tuckerman <brendan.tuckerman@gmail.com>
2022-08-15 14:47:16 +1000

    Completed Ruby conditional exercises

--- week4/conditionals-exercises/air-conditioning.rb
+++ week4/conditionals-exercises/air-conditioning.rb
@@ -7,21 +7,19 @@ def temp_control
     puts "Welcome to the temperature control system.\n Is the A/C working? [y/n]"
     functional =  gets.chomp
     functional.downcase
-    
-    if functional == 'y' || functional == 'yes' || functional == 'n' || functional == 'no' 
-        puts "What is the current temperature?"
-        current_temp = gets.chomp
-        current_temp = current_temp.to_f
-        working = true
-        
-        puts "What temperature would you like it to be? (0-30.0)"
-        desired_temp = gets.chomp
-        desired_temp = desired_temp.to_f
-        
+
+    if functional == 'y' || functional == 'yes'
+        working = true 
+    elsif functional == 'n' || functional == 'no' 
+        working = false
     else
         puts "Please enter a valid response."
         temp_control
     end
+
+    current_temp
+    desired_temp
+    
     
     if working && current_temp > desired_temp
         puts "Please turn the A/C on."
@@ -33,6 +31,30 @@ def temp_control
         puts "Fix the A/C whenever you can, its cool."
     end
 
+end
+
+def current_temp
+    puts "What is the current temperature?"
+    current_temp = gets.chomp
+    current_temp = current_temp.to_f
+    if current_temp > -50.0 && current_temp < 60.0
+        return current_temp
+    else
+        puts "Please enter a valid temp"
+        current_temp
+    end
+end
+
+def desired_temp
+    puts "What temperature would you like it to be? (0-30.0)"
+    desired_temp = gets.chomp
+    desired_temp = desired_temp.to_f
+    if desired_temp > -50.0 && desired_temp < 60.0
+        return desired_temp
+    else
+        puts "Please enter a valid temp"
+        desired_temp
+    end
 end
 
 temp_control
