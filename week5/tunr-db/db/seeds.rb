e9ea79d4bc4d247b32f8d089f2f35146963da785
Brendan Tuckerman <brendan.tuckerman@gmail.com>
2022-08-25 11:43:21 +1000

    Thurs am

---
+++ week5/tunr-db/db/seeds.rb
@@ -0,0 +1,20 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+
+print "Creating songs"
+
+Song.destroy_all
+
+s1 = Song.create! title: "Achy Breaky Heart"
+s2 = Song.create! title: "Draw Us In"
+s3 = Song.create! title: "Burn the witch"
+s4 = Song.create! title: "Identikit"
+
+
+puts "created #{ Song.count} songs"
